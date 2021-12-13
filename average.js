9990d849e67d1681211dd2486332d179df5404c5
kratijain1606 <kratijain1600@gmail.com>
2021-12-13 17:29:35 +0530

    Chnge defn to cal avg

--- average.js
+++ average.js
@@ -1,6 +1,15 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  let ans = 0;//initialize answer
+  let len = numbers.length;//length of array
+  numbers.map((num,i)=>{
+    if(!isNaN(num)){
+      ans += num; //add value to ans if it is a number
+    }
+    else{
+      len -= 1 //if it is not a number, reduce the length of the array
+    }
+  })
+  return ans/len //return the average
 }
-
-module.exports = {average};
+module.exports = {average};//export the function
